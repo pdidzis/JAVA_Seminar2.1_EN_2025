dd492ad12a37e20b4e3b5d498c798f9b77167e9a
matyss <matissp1302@gmail.com>
2025-02-26 15:31:18 +0200

    Add : Bus class is finished, added toString ect.

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -29,10 +29,29 @@ public class Bus extends Vehicle{
 			
 	}
 	
-	public void hasBaggageDivision(boolean inputHasBaggageDivision) {
+	public void setHasBaggageDivision(boolean inputHasBaggageDivision) {
 		hasBaggageDivision = inputHasBaggageDivision;
 	}
 	
+	
+	//4.default const
+	
+	public Bus() {
+		super(); // it will call no argument const from Vehicle class
+		setNumberOfSeats(10);
+		setHasBaggageDivision(true);
+	}
+	
+	public Bus(String inputTitle,float inputPrice,EnergyType inputEnergyType, int inputNumberOfSeats, boolean inputHasBaggageDivision) {
+		super(inputTitle, inputPrice, inputEnergyType);
+		setNumberOfSeats(inputNumberOfSeats);
+		setHasBaggageDivision(inputHasBaggageDivision);
+		
+	}
+	
+	public String toString() {
+		return super.toString() + ", NumberOfSeats : " + numberOfSeats + ", has baggage: " + hasBaggageDivision;
+	}
 	
 	
 	
